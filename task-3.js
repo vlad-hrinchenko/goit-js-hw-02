caf02cf10c432c2a2a8b8f4db049c96a55928713
Vlad Hrinchenko <vladuslavhrinchenko@gmail.com>
2025-01-27 12:04:33 +0200

    add homework task

---
+++ task-3.js
@@ -0,0 +1,22 @@
+function checkForSpam(message) {
+  const forbiddenWords = ['spam', 'sale'];
+  const lowerCaseMessage = message.toLowerCase();
+
+  for (let word of forbiddenWords) {
+    if (lowerCaseMessage.includes(word)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
